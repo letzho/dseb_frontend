40dc8a8f2bae5d8cda604a5511728a3e365ab925
letzho <122744653+letzho@users.noreply.github.com>
2025-01-01 14:13:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Change 'dist' to 'build' if needed
+    outDir: 'build' // Change 'dist' to 'build' if needed
   }
 })
